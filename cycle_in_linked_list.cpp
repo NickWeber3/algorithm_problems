44b8bfa4450618af89d4ba7977788660c928aeee
Nick Weber <webenic3@gmail.com>
2017-09-30 16:22:10 -0400

    Find start of cycle in linked list, if it exists
    Given a linked list, return the node where the cycle begins. If there is no cycle, return null.

--- cycle_in_linked_list.cpp
+++ cycle_in_linked_list.cpp
@@ -5,3 +5,61 @@
 #                           \/    |
 #                 1 -> 2 -> 3 -> 4
 #                                       Return the node corresponding to node 3. 
+
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          V
+# Solution:
+
+/*
+ * Definition for singly-linked list in C++:
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+ListNode* Solution::detectCycle(ListNode* A) {
+    ListNode *skip_one = A, *skip_two = A;
+    bool isCycle = false;
+    while (skip_one and skip_two and skip_two->next) {
+        skip_one = skip_one->next;
+        skip_two = skip_two->next->next;
+        if (skip_one == skip_two) {isCycle = true; break;}
+    }
+    if (isCycle == false) {return nullptr;}
+    
+    skip_one = A;
+    while (skip_one != skip_two) {
+        skip_one = skip_one->next;
+        skip_two = skip_two->next;
+    }
+}
+
+# O(N) time, O(1) space
+# Explanation: (update this)
