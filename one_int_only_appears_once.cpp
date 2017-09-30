94736f2dfdedb4c7a1e0718f9d4bb5f9fc17703b
Nick Weber <webenic3@gmail.com>
2017-09-30 14:00:11 -0400

    Find the only unique element in an array of ints

--- one_int_only_appears_once.cpp
+++ one_int_only_appears_once.cpp
@@ -1,34 +1,34 @@
-Given an array of integers, every element appears twice except for one. Find that single one.
-Example:    Input: [1 2 2 3 1] -----> Output: 3
+# Given an array of integers, every element appears twice except for one. Find that single one.
+# Example:    Input: [1 2 2 3 1] -----> Output: 3
 
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          |
-                                          V
-Solution:
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          V
+# Solution:
 
 int singleNumber(const vector<int> &A) {
     int answer = 0;
@@ -37,3 +37,8 @@ int singleNumber(const vector<int> &A) {
     }
     return answer;
 }
+
+# Explanation: keep track of a running sum of the bit-wise XORs of the elements in the array. This will identify the 
+# one unique number inside. In other words, for any integer A, A^A will always be 0, because each bit place will of the
+# result will be 0. Only one element in the array will not have its bit places cancelled, and that is the unique one.
+# Note: this only works because all duplicates only appear TWICE!
