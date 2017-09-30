0a89442def49426d39a9c736496e84ebcb7f7bce
Nick Weber <webenic3@gmail.com>
2017-09-30 14:52:16 -0400

    Find number that appears floor(n/2) times in array
    Given an array of size n, find the majority element. The majority element is the element that appears more than floor(n/2

--- find_the_majority.cpp
+++ find_the_majority.cpp
@@ -1,4 +1,39 @@
-int Solution::majorityElement(const vector<int> &A) {
+# Given an array of size n, find the majority element. The majority element is the element that appears more than floor(n/2) times.
+# You may assume that the array is non-empty and the majority element always exist in the array.
+# Example:
+#               Input:      [2, 1, 2]
+#               Return:     2 which occurs 2 times which is greater than 3/2.
+
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          V
+# Solution:
+
+int majorityElement(const vector<int> &A) {
     int majority_elem = A[0], net_majority_occurances = 1;
     for (int i = 1; i < int(A.size()); ++i) {
         (A[i] == majority_elem) ? ++net_majority_occurances : --net_majority_occurances;
@@ -9,3 +44,6 @@ int Solution::majorityElement(const vector<int> &A) {
     }
     return majority_elem;
 }
+
+# O(n) time, O(1) memory
+# Explanation: (update this) this is the coolest problem
