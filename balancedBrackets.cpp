4b892a47a8c204928ccc4e3f658d64e7f014dbce
Nick Weber <webenic3@gmail.com>
2017-11-04 18:58:13 -0400

    Is this string's ( ), { }, and [ ] balanced?

--- balancedBrackets.cpp
+++ balancedBrackets.cpp
@@ -1,1 +1,66 @@
-// Write a function that 
+# Make sure that the parentheses, braces,and brackets of a string are balanced
+# Example:
+#          "{ [ ] ( ) }" should return true
+#          "{ [ ( ] ) }" should return false
+#          "{ [ }"       should return false
+#
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          |
+#                                          V
+# Solution:
+
+bool bracketValidator(string &s) {
+    
+    stack<char> openers;
+    
+    for (char c : s) {
+        
+        if (c == '(' ||
+            c == '{' ||
+            c == '[') {
+            openers.push(c);
+        }
+        
+        else if (c == ')' &&
+                 openers.top() == '(') {
+            openers.pop();
+        }
+        
+        else if (c == '}' &&
+                 openers.top() == '{') {
+            openers.pop();
+        }
+        
+        else if (c == ']' &&
+                 openers.top() == '[') {
+            openers.pop();
+        }
+        
+    }
+    
+    return openers.empty();
+}
