e953dd40672160b23ced78ecb6faac6857f72f5c
Nick Weber <webenic3@gmail.com>
2017-11-08 22:05:52 -0500

    Find integer that appears n/2 times in array

--- FindTheMajority.cpp
+++ FindTheMajority.cpp
@@ -1,37 +1,37 @@
-# Given an array of size n, find the majority element. The majority element is the element that appears more than floor(n/2) times.
-# You may assume that the array is non-empty and the majority element always exist in the array.
-# Example:
-#               Input:      [2, 1, 2]
-#               Return:     2 which occurs 2 times which is greater than 3/2.
+// Given an array of size n, find the majority element. The majority element is the element that appears more than floor(n/2) times.
+// You may assume that the array is non-empty and the majority element always exist in the array.
+// Example:
+//               Input:      [2, 1, 2]
+//               Return:     2 which occurs 2 times which is greater than 3/2.
 
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          |
-#                                          V
-# Solution:
+/*                                        |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          V
+*/
 
 int majorityElement(const vector<int> &A) {
     int majority_elem = A[0], net_majority_occurances = 1;
