da82122acd2d0b0960410db65a4b4e0229023190
Nick Weber <webenic3@gmail.com>
2017-11-08 22:12:27 -0500

    Reverse an integers digits

---
+++ Algorithm and Complexity/ReverseInteger.cpp
@@ -0,0 +1,48 @@
+// Reverse digits of an integer
+// Example:
+//          x = 123, return 321
+//          x = -123, return -321
+
+/*                                        |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          |
+                                          V
+*/
+
+int reverse_int(int num) {
+    int reversed = 0, rem;
+    while (num != 0) {
+        rem = num % 10;
+        reversed = (reversed*10) + rem;
+        num /= 10;
+    }
+    return reversed;
+}
+
+# O(n) time, O(1) space
+# Explanation: Start by taking remander of number when divided by ten, giving you the digit in the ones place. Multiply the
+# reversed number by 10 to provide room in the ones place of the reverse number, and then add the remander into that 
+# ones place. Do this until the original number has no digits left.
