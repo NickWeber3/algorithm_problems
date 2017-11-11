834fc7280d7797fb552a51193a51cb3c793a2dde
Nick Weber <webenic3@gmail.com>
2017-11-11 13:13:05 -0500

    Create directedGraphRoute.cpp

---
+++ Trees And Graphs/directedGraphRoute.cpp
@@ -0,0 +1,13 @@
+/***
+    Problem:
+    Given a directed graph, design an algorithm to find out whether there is a route
+    between two nodes.
+***/
+
+
+bool isThereARoute() {
+
+}
+
+
+// FIND DIRECTED GRAPH REPRESENTATION, COME BACK
